9f1dc2ea65697ac115f50b53d13d9601442214ef
Jinchao Ye <yjc111@gmail.com>
2012-08-12 09:45:59 -0700

    map

---
+++ www/js/getUserLocation.php
@@ -0,0 +1,20 @@
+<?php
+  $dbname = "./SportsNearby.db";
+  try {
+    $db = new PDO("sqlite:" . $dbname);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $e) {
+    "SQLite connection failed: " . $e->getMessage();
+    exit();
+  }
+  $result = db->query("select * from User where uID = '".$_GET[userid]."'");
+  $db = null;
+  foreach($result as $row){
+  	$lat = $row['uLocation_a'];
+  	$lon = $row['uLocation_b'];
+  }
+  echo $lat;
+  echo $lon;
+  $arr = array('lat' => $lat, 'lon' => $lon);
+  echo json_encode($arr);
+?>
