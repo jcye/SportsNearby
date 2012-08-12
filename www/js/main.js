c16f4b7433f15edab82567a548e683c22c0037e3
nyn531 <nyn531@fb.com>
2012-08-11 21:57:09 -0700

    update ui

---
+++ www/js/main.js
@@ -0,0 +1,19 @@
+$("#page-event").live('pagebeforeshow', function(){
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"),
+    mapOptions);
+})
+
+$("#page-create").live('pagebeforeshow', function(){
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas_small"),
+    mapOptions);
+})
