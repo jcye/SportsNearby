f8f08d00e9e529fa67050bc491ab68561c801489
Jinchao Ye <yjc111@gmail.com>
2012-08-12 08:23:13 -0700

    map

--- www/js/main.js
+++ www/js/main.js
@@ -2,13 +2,15 @@ $("#page-event").live('pagebeforeshow', function(){
   var mapOptions = {
     center: new google.maps.LatLng(-34.397, 150.644),
     zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    callback: function(){
+      this.addMarker({'position': '-34.397, 150.644','icon':'images/basketball.png' }).click(function() {
+                        this.openInfoWindow({ 'content': 'hello world' }, this);
+      });
+    }
   };
   var map = new google.maps.Map(document.getElementById("map_canvas"),
     mapOptions);
-  map.addMarker({'position': '-34.397, 150.644','icon':'images/basketball.png' }).click(function() {
-    map.openInfoWindow({ 'content': 'Hello World'names'' }, map);
-  });
 })
 
 $("#page-create").live('pagebeforeshow', function(){
